ad0bb369ff638d8a387c7468048355f87a48e05a
2003GG <mohamadzat6@gmail.com>
2026-01-21 11:45:26 +0100

    push

---
+++ app/Core/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    private static $instance = null;
+    private $conn;
+    private $host = 'localhost';
+    private $user = 'root';
+    private $db = 'CareerLink';
+    private $password = '';
+
+    private function __construct()
+    {
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $erorr) {
+            die("Database Connection Failed: " . $erorr->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
